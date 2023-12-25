c5a83dd3d0200da850784f53d29f734da4c25b88
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-25 23:18:00 +0200

    added the database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
-
+import { Mongoose } from 'mongoose';
 import { PORT, MONGODBURL } from './config.js';
+import { error } from 'console';
 
 const app = express();
 
@@ -12,3 +13,11 @@ app.get('/', (request, response) => {
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 });
+
+Mongoose
+    .connect(MONGODBURL)
+    .then((result) => {
+        console.log('App successfully connected to the database');
+    }).catch((err) => {
+       console.log(error); 
+    });
