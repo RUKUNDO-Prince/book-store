fe7d49d5f274495cfe58f3f9f2e1bf600b2410d4
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-25 22:54:18 +0200

    sent the first request, and got the response

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send("Hello MERN Stack");
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 });
