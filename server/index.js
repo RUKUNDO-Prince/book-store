cc74578820ef866d3af593fb9d7ab46784c9c818
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-26 22:32:51 +0200

    some changes to getting all books, heading to getting a single book

--- server/index.js
+++ server/index.js
@@ -42,7 +42,10 @@ app.get('/books', async (request, response) => {
     try {
         const books = await Book.find({});
 
-        return response.status(200).send(books);
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
