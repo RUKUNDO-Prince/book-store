b8536efcf00df4695a26dc3829d98848339a84e9
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-26 22:09:54 +0200

    finished creating the new books model, heading to testing

--- server/index.js
+++ server/index.js
@@ -15,8 +15,19 @@ app.get('/', (request, response) => {
 app.post('/books', async (request, response) => {
     try {
         if (!request.body.title || !request.body.author || !request.body.publishYear) {
-            console.log('Hello world');
+            return response.status(400).send({
+                message: "Send all required fields: title, author and publish year!",
+            });
         }
+
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        response.status(201).send(book);
+
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
