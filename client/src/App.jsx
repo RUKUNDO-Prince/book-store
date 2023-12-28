d41dc74d9768baaa3a49a5e626f7c87100be0ec5
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-28 23:47:15 +0200

    finished routing, heading to displaying the books

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { Home, CreateBooks, EditBook, DeleteBooks, ShowBooks } from './pages/index';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBooks />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBooks />} />
     </Routes>
   )
 }
