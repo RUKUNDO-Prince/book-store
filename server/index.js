3ac198fc4db53880aadcbece4a7658f518faf110
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-28 22:41:35 +0200

    finished the backend of the application, heading to the frontend

--- server/index.js
+++ server/index.js
@@ -15,13 +15,13 @@ app.use(express.json());
 // METHOD 1
 app.use(cors());
 // METHOD 2
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (request, response) => {
     console.log(request);
