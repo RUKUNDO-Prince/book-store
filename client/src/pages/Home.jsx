49ffde6e14bbff9713b452b39a03b90b27b8512f
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-29 00:13:54 +0200

    making the homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Spinner from '../components/Spinner'
+import Link from 'react-router-dom'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('https://book-store-yu6m.onrender.com/books')
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+  
+    return () => {
+      second
+    }
+  }, [third])
+  
+
   return (
     <div>
       Home
