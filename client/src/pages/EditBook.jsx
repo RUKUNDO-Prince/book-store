06f038c69803f07040c9917e3ae142c0bc460832
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-30 19:18:11 +0200

    added notistack to the project

--- client/src/pages/EditBook.jsx
+++ client/src/pages/EditBook.jsx
@@ -3,7 +3,7 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-// import { useSnackbar } from 'notistack';
+import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
   const [title, setTitle] = useState('');
