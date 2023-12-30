b33d88790c3bd2c9e6db0846eacafd07e8c38e6b
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-30 16:48:10 +0200

    showing a single book

--- client/src/pages/ShowBooks.jsx
+++ client/src/pages/ShowBooks.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
 
 const ShowBooks = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+    // .get(`https://book-store-yu6m.onrender.com/books/${id}`)
+    .get(`http://localhost:5555/books/${id}`)
+    .then((response) => {
+      setBook(response.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  }, []);
+
   return (
     <div>
       ShowBooks
