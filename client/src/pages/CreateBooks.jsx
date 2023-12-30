14f844464e7b9a202178a92f9523344f7a9fe708
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-12-30 18:40:07 +0200

    creating a book

--- client/src/pages/CreateBooks.jsx
+++ client/src/pages/CreateBooks.jsx
@@ -31,8 +31,26 @@ const CreateBooks = () => {
   };
 
   return (
-    <div>
-      CreateBooks
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create Book</h1>
+      {
+        loading ? <Spinner /> : ''
+      }
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label htmlFor="" className='text-xl mr-4 text-gray-500'>Title</label>
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full ' />
+        </div>
+        <div className='my-4'>
+          <label htmlFor="" className='text-xl mr-4 text-gray-500'>Auuthor</label>
+          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full ' />
+        </div>
+        <div className='my-4'>
+          <label htmlFor="" className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input type="text" value={publishYear} onChange={(e) => setPublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full ' />
+        </div>
+      </div>
     </div>
   )
 }
