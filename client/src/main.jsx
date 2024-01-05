09585d96eb9d63f20efa1dda61c8c7e3e1230924
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-01-05 15:19:41 +0200

    full project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
@@ -10,5 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <SnackbarProvider>
       <App />
     </SnackbarProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
--- server/index.js
+++ server/index.js
@@ -1,43 +1,41 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
 import { PORT, MONGODBURL } from './config.js';
-import { error } from 'console';
-import { Book } from './models/bookModel.js';
-import bookRoute from './routes/booksRoutes.js'
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 
 const app = express();
 
-// MIDDLEWARE FOR PARSING THE REQUEST BODY
+// Middleware for parsing request body
 app.use(express.json());
 
-// MIDDLEWARE FOR HANDLING CORS POLICY
-// METHOD 1
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
-// METHOD 2
+// Option 2: Allow Custom Origins
 // app.use(
-//     cors({
-//         origin: 'http://localhost:3000',
-//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//         allowedHeaders: ['Content-Type'],
-//     })
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
 // );
 
 app.get('/', (request, response) => {
-    console.log(request);
-    return response.status(234).send("Hello MERN Stack");
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-app.use('/books', bookRoute);
+app.use('/books', booksRoute);
 
 mongoose
-    .connect(MONGODBURL)
-    .then((result) => {
-        console.log('App successfully connected to the database');
-        
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`);
-        });
-    }).catch((err) => {
-       console.log(error); 
+  .connect(MONGODBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
